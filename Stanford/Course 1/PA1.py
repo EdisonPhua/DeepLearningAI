c751285f23deb136be497b9d13baf05d38e42d12
EdisonPhua <edison.phua2004@gmail.com>
2023-03-20 14:17:48 +0000

    Mon, Mar 20, 2023, 10:17 PM +08:00

--- Stanford/Course 1/PA1.py
+++ Stanford/Course 1/PA1.py
@@ -26,7 +26,10 @@ def image2vector(image):
     v = image.reshape(image.shape[0]*image.shape[1]*image.shape[2], 1)
     return v
 
-image = np.array([[[ 0.67826139,  0.29380381],
+image = np.array(
+    [
+    
+    [   [ 0.67826139,  0.29380381],
         [ 0.90714982,  0.52835647],
         [ 0.4215251 ,  0.45017551]],
 
