863bf64193ae95f938942b8c428fcb59cb45cb2b
EdisonPhua <edison.phua2004@gmail.com>
2023-03-20 14:40:09 +0000

    Mon, Mar 20, 2023, 10:40 PM +08:00

--- Stanford/Course 1/PA1.py
+++ Stanford/Course 1/PA1.py
@@ -90,6 +90,10 @@ def L2(yhat, y):
     """
     
     ### START CODE HERE ### (≈ 1 line of code)
-    
+    loss = np.sum((y-yhat)**2)
+    return loss
     ### END CODE HERE ###
     
+yhat = np.array([.9, 0.2, 0.1, .4, .9])
+y = np.array([1, 0, 0, 1, 1])
+print("L2 = " + str(L2(yhat,y)))
