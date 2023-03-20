12bd39616fb9cdd4b29ff63b86f6c1c2dff587c6
EdisonPhua <edison.phua2004@gmail.com>
2023-03-20 14:14:30 +0000

    Mon, Mar 20, 2023, 10:14 PM +08:00

--- Stanford/Course 1/PA1.py
+++ Stanford/Course 1/PA1.py
@@ -23,8 +23,8 @@ def image2vector(image):
     Returns:
     v -- a vector of shape (length*height*depth, 1)
     """
-    
-    
+    v = image.reshape(image.shape[0]*image.shape[1]*image.shape[2], 1)
+    return v
 
 
 
