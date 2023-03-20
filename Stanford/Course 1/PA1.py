9aaa073dceb0df2071f31c5389207fac8fabc5b5
EdisonPhua <edison.phua2004@gmail.com>
2023-03-20 13:42:20 +0000

    Mon, Mar 20, 2023, 9:42 PM +08:00

--- Stanford/Course 1/PA1.py
+++ Stanford/Course 1/PA1.py
@@ -1,21 +1,16 @@
-import math
-
-
-def basic_sigmoid(x):
-    
-    x = 1/(1+math.exp(-x))
-    
-    return x
-
-
-print(basic_sigmoid(3))
 
 
 import numpy as np  
 
 def sigmoid(x):
     
-    s 
+    s = 1/(1+np.exp(-x))
+
+    return s
+
+
+
+
     
 
 
