ebc6f1fe3392320c08b9ae1cfc9cd1074200558c
EdisonPhua <edison.phua2004@gmail.com>
2023-03-21 01:56:36 +0000

    Tue, Mar 21, 2023, 9:56 AM +08:00

--- Stanford/Course 1/PA2.py
+++ Stanford/Course 1/PA2.py
@@ -6,5 +6,7 @@ from PIL import Image
 from scipy import ndimage
 from lr_utils import load_dataset
 
+plt.show()
+
 
 train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
